ab5691c9dbe30b61542f7edcf9f6133efb1b4cc7
Elvis Luciano <elvis.gs@gmail.com>
2017-09-09 16:26:05 -0300

    Criação do esqueleto do projeto

---
+++ src/main/java/br/com/eudes/semanticfx/semanticfx/SemanticFxApplication.java
@@ -0,0 +1,12 @@
+package br.com.eudes.semanticfx.semanticfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticFxApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/eudes/semanticfx/semanticfx/SemanticFxApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.eudes.semanticfx.semanticfx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemanticFxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
